102f5e360c90d76662429e2bed85babd783e052a
dixitt5 <tilajidixit@gmail.com>
2023-09-30 15:11:04 +0530

    firebase setup and form is done mostly

---
+++ src/config/firebase.js
@@ -0,0 +1,26 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECTID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_SENDERID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APPID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENTID,
+};
+
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+const storage = getStorage(app);
+
+export { db, storage };
--- src/pages/createnft/index.js
+++ src/pages/createnft/index.js
@@ -1,108 +1,124 @@
-import React from 'react'
+import React, { useRef, useState } from "react";
+import { db } from "@/config/firebase";
+import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
+import { v4 } from "uuid";
+import { storage } from "@/config/firebase";
+import { addDoc, collection, setDoc } from "firebase/firestore";
 
 const Create = () => {
+  const nameRef = useRef();
+  const priceRef = useRef();
+  const descriptionRef = useRef();
+  const linkRef = useRef();
+  const [imageUpload, setImageUpload] = useState();
+  const [Image, setImage] = useState();
 
+  const uploadFile = async () => {
+    const imageRef = ref(storage, `images/${v4()}`);
+    await uploadBytes(imageRef, imageUpload);
+    const downloadUrl = await getDownloadURL(imageRef);
+    linkRef.current = downloadUrl;
+  };
+
+  const listNFT = async (e) => {
+    e.preventDefault();
+    await uploadFile();
+    const formData = {
+      name: nameRef.current.value,
+      price: priceRef.current.value,
+      descrtiption: descriptionRef.current.value,
+      link: linkRef.current,
+    };
+    console.log(formData);
+    const NFTDocRef = collection(db, "nfts");
+    setDoc(NFTDocRef, formData);
+    alert("done");
+  };
   return (
-    <div><div className="flex flex-col place-items-center mt-5" id="nftForm">
-    <form className="bg-black shadow-pink-800 rounded px-8 pt-4 pb-8 mb-4">
-      <h3 className="text-center font-bold text-white bg-blue-400 rounded-md p-2 mb-8">
-        Upload the NFT
-      </h3>
-      <div className="mb-4">
-        <label
-          className="block text-blue-500 text-sm font-bold mb-2"
-          htmlFor="name"
+    <div>
+      <div className="flex flex-col place-items-center mt-5" id="nftForm">
+        <form
+          onSubmit={listNFT}
+          className="bg-black shadow-pink-800 rounded px-8 pt-4 pb-8 mb-4"
         >
-          NFT Title
-        </label>
-        <input
-          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-          id="name"
-          type="text"
-          placeholder="Flow"
-          // onChange={(e) =>
-          //   updateFormParams({ ...formParams, name: e.target.value })
-          // }
-          // value={formParams.name}
-        ></input>
-      </div>
-      <div className="mb-6">
-        <label
-          className="block text-blue-500 text-sm font-bold mb-2"
-          htmlFor="description"
-        >
-          NFT Description
-        </label>
-        <textarea
-          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-          cols="40"
-          rows="5"
-          id="description"
-          type="text"
-          placeholder="More description about NFT"
-          // value={formParams.description}
-          // onChange={(e) =>
-          //   updateFormParams({ ...formParams, description: e.target.value })
-          // }
-        ></textarea>
-      </div>
-      <div className="mb-4">
-        <label
-          className="block text-blue-500 text-sm font-bold mb-2"
-          htmlFor="price"
-        >
-          Price
-        </label>
-        <input
-          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-          id="number"
-          type="number"
-          placeholder="Price"
-          // onChange={(e) => {
-          //   updateFormParams({ ...formParams, email: e.target.value });
-          //   store.dispatch(updateEmail({ email: formParams.email }));
-          // }}
-          // value={formParams.email}
-        ></input>
-      </div>
-      {/* <div className="mb-6">
-        <label
-          className="block text-pink-500	 text-sm font-bold mb-2"
-          htmlFor="price"
-        >
-          Price (in ETH)
-        </label>
-        <input
-          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-          type="number"
-          placeholder="Min 0.01 ETH"
-          step="0.01"
-          value={formParams.price}
-          onChange={(e) =>
-            updateFormParams({ ...formParams, price: e.target.value })
-          }
-        ></input>
-      </div> */}
-      <div>
-        <label
-          className="block text-blue-500 text-sm font-bold mb-2"
-          htmlFor="image"
-        >
-          Upload NFT Image
-        </label>
-        <input type={"file"}></input>
-      </div>
-      <br></br>
-      <div className="text-green text-center">heif</div>
-      <button
-        onClick={listNFT}
-        className="font-bold mt-10 w-full bg-green-500 text-white rounded p-2 shadow-lg"
-      >
-        List NFT
-      </button>
-    </form>
-  </div></div>
-  )
-}
+          <h3 className="text-center font-bold text-white bg-blue-400 rounded-md p-2 mb-8">
+            Upload the NFT
+          </h3>
+          <div className="mb-4">
+            <label
+              className="block text-blue-500 text-sm font-bold mb-2"
+              htmlFor="name"
+            >
+              NFT Title
+            </label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="name"
+              type="text"
+              placeholder="Flow"
+              ref={nameRef}
+            ></input>
+          </div>
+          <div className="mb-6">
+            <label
+              className="block text-blue-500 text-sm font-bold mb-2"
+              htmlFor="description"
+            >
+              NFT Description
+            </label>
+            <textarea
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              cols="40"
+              rows="5"
+              id="description"
+              type="text"
+              placeholder="More description about NFT"
+              ref={descriptionRef}
+            ></textarea>
+          </div>
 
-export default Create
+          <div className="mb-6">
+            <label
+              className="block text-blue-500 text-sm font-bold mb-2"
+              htmlFor="price"
+            >
+              Price (in Matic)
+            </label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              type="number"
+              placeholder="Price of NFT"
+              step="0.01"
+              ref={priceRef}
+            ></input>
+          </div>
+          <div>
+            <label
+              className="block text-blue-500 text-sm font-bold mb-2"
+              htmlFor="image"
+            >
+              Upload NFT Image
+            </label>
+            <input
+              type={"file"}
+              onChange={(event) => {
+                setImageUpload(event.target.files[0]);
+                setImage(URL.createObjectURL(event.target.files[0]));
+              }}
+            ></input>
+          </div>
+          <br></br>
+          <div className="text-green text-center">List you NFT here!</div>
+          <button
+            type="submit"
+            className="font-bold mt-10 w-full bg-green-500 text-white rounded p-2 shadow-lg"
+          >
+            List NFT
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Create;
